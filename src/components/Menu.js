67ed58129182182f036f94e428432296fffca8b0
dianannperla <waafirmelyza@hotmail.com>
2025-09-02 17:31:23 +0100

    feat: initial commit for src/components/Menu.js

---
+++ src/components/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Menu() {
+  return (
+    <div>
+      <h2>Thực Đơn Của Bạn</h2>
+      {/* Danh sách món ăn sẽ được hiển thị tại đây */}
+    </div>
+  );
+}
+
+export default Menu;

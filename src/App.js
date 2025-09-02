fa1ca433443fd39c46b0d1a9f0160d29731db1b2
dianannperla <waafirmelyza@hotmail.com>
2025-09-02 17:31:21 +0100

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Menu from './components/Menu';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Thực Đơn Thông Minh</h1>
+      <Menu />
+    </div>
+  );
+}
+
+export default App;

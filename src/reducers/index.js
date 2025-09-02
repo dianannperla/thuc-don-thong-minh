4a95df12a0ddf40995c9ad9dc25f83c529a3658e
dianannperla <waafirmelyza@hotmail.com>
2025-09-02 17:31:24 +0100

    feat: initial commit for src/reducers/index.js

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+// Nhập các reducer khác nếu cần.
+
+const rootReducer = combineReducers({
+  // Thêm các reducer ở đây.
+});
+
+export default rootReducer;
